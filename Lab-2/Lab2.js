76b00cf87053d3fdab681dafed84b6fc4f2b3c0f
adityak0711 <52545282+adityak0711@users.noreply.github.com>
2020-09-29 13:38:53 +0530

    Update Lab2.js

--- Lab-2/Lab2.js
+++ Lab-2/Lab2.js
@@ -30,10 +30,10 @@ db.Employee.find()
 db.Department.find()
 
 //select emp name and dept num where dept num is between 1001 and 1005
-db.Employee.find({DeptNum: {$gt : 1000, $lt : 1006}},{Name:1,DeptNum:1})
+db.Employee.find({DeptNum: {$gt : 1000, $lt : 1006}},{name:1,DeptNum:1})
 
 //find all employees with age greater than 25
 db.Employee.find({age:{$gt:25}})
 
 //find names of all employees with name starting with A
-db.Employee.find({Name:/^A/})
+db.Employee.find({name:/^A/})
